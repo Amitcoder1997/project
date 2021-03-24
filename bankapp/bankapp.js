41e1e569d772479d024dd9ba94f1b0370802771d
Amitcoder1997 <amitdhumale123@gmail.com>
2021-03-24 11:30:04 +0530

    adding initial files

---
+++ bankapp/bankapp.js
@@ -0,0 +1,10 @@
+AccountHolder = {
+    Name: "amit",
+    PhoneNo: "7894561230",
+    AccountNo: "33887799515",
+    Balance: "50000"
+}
+
+function Balance() {
+    document.AccountHolder.Balance;
+}
